c5a314ae1a64fdc41bd6db5721ea40af81c4f3cf
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-10-03 11:26:34 +0100

    implement Product domain

--- src/main/java/tn/esb/storeApi/Entities/Product.java
+++ src/main/java/tn/esb/storeApi/Entities/Product.java
@@ -1,11 +1,40 @@
 package tn.esb.storeApi.Entities;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.*;
 
-@Entity
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter //genere tous les getters pour tous les attributs lors du runtime
+@Setter //de meme pour les setters
+@NoArgsConstructor //genere un constructeur non parametré lors du runtime
+//@AllArgsConstructor //genere un constructeur avec tous les attributs comme paramètres lors du runtime
+//@EqualsAndHashCode //permet de redifinir les méthodes de la classe Object : equals( compare deux objets) et  hashCode (gnere un code hashage)
+//Deux produits seront identiques s'ils ont une egalité entre tous les champs (deux à deux)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //dans ce cas la comparaison entre deux produits sera efectuée via
+// uniquement les champs marqués avec Include
+//@ToString //permet de redefinir la méthode de la classe Object "toStrin()"
+//retourne les valeurs de tous les attributs (id, name, ... image)
+@ToString(exclude = {"image","description"})//retourne tous les attributs sauf image et description
+@Entity //La classe product sera transformée en une table relationnelle par l'ORM
+//ORM : Object Relational Mapping
 public class Product {
-    @Id
+    @Id // l'attribut id est clé primaire
+    //@GeneratedValue //la clé primaire est automatiquement generée
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // la première valeur de clé
+    //attribuée sera 1, la 2ème 2, ....
     private Long id;
+    @EqualsAndHashCode.Include
+    private String name;
+    private BigDecimal price;
+    @EqualsAndHashCode.Include
+    private LocalDate fabricationDate;
+    private LocalDate expirationDate;
+    private String description;
+    private int stock;
+    @Lob
+    private byte[] image;
+
 }
