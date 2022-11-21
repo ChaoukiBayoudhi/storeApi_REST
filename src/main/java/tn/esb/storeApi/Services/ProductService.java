f8de275f5f0724940b9ff0ceeb03aeab37d73f4f
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-11-21 10:34:19 +0100

    implement Services and Controllers

--- src/main/java/tn/esb/storeApi/Services/ProductService.java
+++ src/main/java/tn/esb/storeApi/Services/ProductService.java
@@ -1,6 +1,8 @@
 package tn.esb.storeApi.Services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import tn.esb.storeApi.Entities.Product;
 import tn.esb.storeApi.Repositories.ProductRepository;
@@ -13,22 +15,23 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
-    public List<Product> getAll()
-    {
-        return getRepositoryAll();
-    }
 
-    private List<Product> getRepositoryAll() {
-        return repository.findAll();
+    public ResponseEntity<?> getAll() {
+        List<Product> res=repository.findAll();
+        if(res.isEmpty())
+            //return ResponseEntity.noContent().build();
+            //or
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("There are no products available");
+        return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 
     //une mauvaise implementation
     //on l'ameliore via l'utilisation de ResponseEntity
-    public Product getProduct(Long id)
+    public ResponseEntity<?> getProduct(Long id)
     {
         Optional<Product> res= repository.findById(id);
         if (res.isPresent())
-            return res.get();
-        return null;
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(res.get());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("There is no product available with id = " + id);
     }
 }
--- src/main/java/tn/esb/storeApi/Web/ProductController.java
+++ src/main/java/tn/esb/storeApi/Web/ProductController.java
@@ -1,6 +1,7 @@
 package tn.esb.storeApi.Web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping("/all")
-    public List<Product> getProducts() {
+    public ResponseEntity<?> getProducts() {
         return service.getAll();
     }
     @GetMapping("/product/{id}")
